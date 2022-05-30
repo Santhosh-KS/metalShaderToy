3113a7ecec0cbebb9b1311bac9c3b250a2670d82
santMan <santhoshachar08@gmail.com>
2022-05-30 16:27:47 +0530

    Initial Commit

---
+++ shaderBase/shaderBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  shaderBaseApp.swift
+//  shaderBase
+//
+//  Created by Santhosh K S on 30/05/22.
+//
+
+import SwiftUI
+
+@main
+struct shaderBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
